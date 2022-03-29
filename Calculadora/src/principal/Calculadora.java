43fb29e19ceed760b9684008b5051a5ade3bf2f0
irakaslea <irakaslea@DESKTOP-UFLG394>
2022-03-29 12:12:06 +0200

    dldkfs

--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -25,7 +25,10 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
+import logs.FiltroLogMultiplicar;
+import logs.FormatoHTML;
 import logs.LogConsolaMasFichero;
 import menu.Menu;
 import operaciones.Operaciones;
@@ -35,37 +38,14 @@ public class Calculadora{
 	 private static final Logger LOGGER = Logger.getLogger(Calculadora.class.getName());
 	 
 	 public static void main(String[] args) {   
+		 configurarLog();
+		 
         int resultado = 0;
         String operacion = "";
         int[] operandos = new int [2];
         
         Menu menu = new Menu();
         Operaciones operaciones = new Operaciones();
-        
-        Handler fileHandler=null;
-        Handler consoleHandler = null;
-
-        try {
-            fileHandler  = new FileHandler("./logs/operaciones.log");
-            consoleHandler = new ConsoleHandler();
-
-        } catch(IOException exception){
-            LOGGER.log(Level.SEVERE, "Ocurrió un error en FileHandler.", exception);
-        }
-   
-        //Evita que muestre algo por consola
-        LogManager.getLogManager().reset();
-        
-        
-        //Asignar handlers al objeto LOGGER
-        LOGGER.addHandler(consoleHandler);
-        LOGGER.addHandler(fileHandler);
-        
-        //Establecer niveles a handlers y LOGGER
-        consoleHandler.setLevel(Level.WARNING);
-        fileHandler.setLevel(Level.FINE);
-        LOGGER.setLevel(Level.FINE);//O ALL
-
         
         do{
             operandos = menu.pedirNumeros();
@@ -97,4 +77,37 @@ public class Calculadora{
             }
         }   while (menu.repetir());
     }
+	 
+	 public static void configurarLog() {
+	        //Evita que muestre algo por consola
+		 	LOGGER.setUseParentHandlers(false); 
+	        
+	        Handler fileHandler=null;
+	        Handler consoleHandler = new ConsoleHandler();
+	        
+	        //asociar
+	        LOGGER.addHandler(consoleHandler);
+
+
+	        try {
+	            fileHandler  = new FileHandler("./logs/LOGoperaciones.html");//con true le añade contenido
+	            consoleHandler = new ConsoleHandler();
+
+	        } catch(IOException exception){
+	            LOGGER.log(Level.SEVERE, "Ocurrió un error en FileHandler.", exception);
+	        }
+	        
+	        //implementamos el formateo de salida y lo añadimos
+	        fileHandler.setFormatter(new FormatoHTML());
+	        LOGGER.addHandler(fileHandler);
+
+	        
+	        //Establecer niveles a handlers y LOGGER
+	        consoleHandler.setLevel(Level.WARNING);
+	        fileHandler.setLevel(Level.FINE);
+	        
+	        fileHandler.setFilter(new FiltroLogMultiplicar());
+	        
+	        LOGGER.setLevel(Level.FINE);//O ALL
+	 }
 }
